b6c44d4323c2c2bef9f400f5e44ecc6794071b08
AidaDaiyrbekova <hgirl0108@gmail.com>
2025-07-07 15:20:14 +0600

    dz

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,33 @@
-// MOVE BLOCK HW 1.2
+const gmailInput = document.querySelector("#gmail_input")
+const gmailButton = document.querySelector("#gmail_button")
+const gmailResult = document.querySelector("#gmail_result")
 
-const parentBlock = document.querySelector('.parent_block');
-const childBlock = document.querySelector('.child_block');
+const regExp = /^(?=[a-zA-Z0-9]{5,})[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9]*@[^\s@]+\.[a-z]{2,3}$/
 
-let positionX = 0
-let positionY = 0
-
-const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
-const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
-
-const moveBlock = () => {
-    if (positionX < offWidth) positionX++
-    if (positionX >= offWidth && positionY < offHeight) positionY++
-    childBlock.style.left = `${positionX}px`
-    childBlock.style.top = `${positionY}px`
-    requestAnimationFrame(moveBlock)
+gmailButton.onclick = () => {
+    if(regExp.test(gmailInput.value)){
+        gmailResult.innerHTML = "OK"
+        gmailResult.style.color = "green"
+    }else {
+        gmailResult.innerHTML = "ERROR"
+        gmailResult.style.color = "red"
+    }
 }
 
-moveBlock();
+const childBlock = document.querySelector(".child_block")
+const parentBlock = document.querySelector(".parent_block")
+
+
+let step = 0;
+
+const moveRedBlock = () => {
+    step++;
+    childBlock.style.left = `${step}px`
+
+    if (step < parentBlock.offsetWidth - childBlock.offsetWidth) {
+        requestAnimationFrame(moveRedBlock)
+    }
+};
+
+requestAnimationFrame(moveRedBlock)
+
